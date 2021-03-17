e8f38d32becf3418a9bbcc6dd1fdbc7d84b7a595
Swaroop <breadfan18@gmail.com>
2021-03-16 23:52:13 -0400

    done with rendering data on the dom

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 // Constant Variables
+const BASE_URL = 'https://api.spacexdata.com/v3/launches?limit=12';
 
 // State Variables
+let launches;
 
-// Cached element references --> parts pf the dom we need to touch. 
+// Cached element references --> parts of the dom we need to touch. 
+const $launches = $('section#launches');
 
 
 // Event Listeners 
 
 
 // Functions
+init();
+
+function init() {
+    getData();
+}
 
 function getData() {
-    $.ajax('https://api.spacexdata.com/v3/launches')
+    $.ajax(BASE_URL)
         .then(
             function (data) {
+                launches = data;
                 console.log(data);
+                render();
             },
             function error() {
                 console.log(error);
@@ -22,4 +32,16 @@ function getData() {
         );
 }
 
-getData();
+function render() {
+    const html = launches.map(function (launch) {
+        return `
+            <article class="card">
+            <h1>${launch.mission_name}</h1>
+            <p>${launch.launch_year}</p>
+            </article>
+        `;
+    })
+
+    $launches.append(html);
+    
+}
